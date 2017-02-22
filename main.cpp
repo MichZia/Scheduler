a7ed194ef8b89b9edce1e838ed42aac61a8d5c46
MichZia <michel.ziakovic@gmail.com>
2017-02-22 16:28:35 -0500

    Introducing detection of forbidden courses and compare with brute force version for non regression purpose. Generation of some pseudo random non trivial examples.
    timing of version with & without forbidden courses show roughly 30% better time.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include <map>
 #include <set>
 #include <iostream>
+#include <random>
 #include <cassert>
+#include <string>
 
 #define POSTCONDITION(c) assert(c)
 
@@ -20,14 +22,21 @@ struct Scheduler
     using Reservation = pair<Room,Timeslice>;
     using Schedule = map<Reservation,Course>;
 
-    Scheduler(Preferences&& preferences_, Room max_rooms_, Timeslice max_timeslice_):
+    Scheduler(Preferences&& preferences_, Room max_rooms_, Timeslice max_timeslice_,bool option_check_forbidden_):
+        option_check_forbidden(option_check_forbidden_),
         preferences(preferences_),
         max_rooms(max_rooms_),
         max_timeslice(max_timeslice_)
     {
         for(const auto& s : preferences)
             for(const auto& c : s.second)
-                all_courses.insert(c);
+            {
+                auto& forbid=all_courses[c];
+                if (option_check_forbidden)
+                    for(const auto& c1 : s.second)
+                        if (c!=c1)
+                            forbid.insert(c1);
+            }
     }
     enum Pref_Matching
     {
@@ -54,6 +63,7 @@ struct Scheduler
         if (nb_not_seen>0)
             return incomplete;
         return matched;
+
     }
     Pref_Matching student_is_ok(Student s) const
     {
@@ -75,54 +85,97 @@ struct Scheduler
             }
         return cur;
     }
+    void check_coherency()
+    {
+        for(const auto s:schedule)
+        {
+            const auto found=location.find(s.second);
+            assert(found != location.end());
+            assert(found->second == s.first);
+        }
+        for(const auto s:location)
+        {
+            const auto found=schedule.find(s.second);
+            assert(found != schedule.end());
+            assert(found->second == s.first);
+        }
+    }
     void add_course(Course c, Reservation r)
     {
+        //check_coherency();
         schedule[r]=c;
         location[c]=r;
+
+        //check_coherency();
     }
     void remove_course(Course c, Reservation r)
     {
+        //check_coherency();
         schedule.erase(r);
         location.erase(c);
+        //check_coherency();
+    }
+    bool overflow(Reservation r) const
+    {
+        return (r.first >= max_rooms) || (r.second >= max_timeslice) ;
     }
     Reservation next(Reservation r) const
     {
         r.second++;
-        if (r.second >= max_timeslice)
+        if (overflow(r))
         {
             r.second=0;
             r.first++;
         }
         return r;
     }
-    bool last(Reservation r) const
-    {
-        return (r.first >= max_rooms);
-    }
     bool try_here(Reservation r)
     {
-        if (last(r))
+        switch(all_is_ok())
+        {
+        case matched:
+            // cout << *this << endl;
+            // return false; // explore other solutions
+            return true;
+        case missed:
             return false;
+
+        case incomplete:
+            break;
+        }
+
+        if (overflow(r))
+            return false;
+
+
         for(const auto& c: all_courses)
         {
-            if (location.find(c)!=location.end()) // a set of residual courses will be better
+            if (location.find(c.first)!=location.end()) // a set of residual courses will be better
                 continue;
-
-            add_course(c,r);
-            switch(all_is_ok())
+            if (option_check_forbidden)
             {
-            case matched:
-                return true;
-
-            case incomplete:
-                if (try_here(next(r)))
-                    return true;
-                break;
-            case missed:
-                break;
+                if ([&]()
+            {
+                for(int room=0 ; room < max_rooms; room++)
+                    {
+                        auto found = schedule.find(make_pair(room,r.second));
+                        if (found!=schedule.end())
+                        {
+                            Course same_time=found->second;
+                            if (c.second.find(same_time)!=c.second.end())
+                                return true;
+                        }
+                    }
+                    return false;
+                }())
+                continue;
             }
-            remove_course(c,r);
+            add_course(c.first,r);
+            if (try_here(next(r)))
+                return true;
+            remove_course(c.first,r);
         }
+
         return false;
     }
 
@@ -133,18 +186,26 @@ struct Scheduler
         return succeed;
     }
 
+
+    bool option_check_forbidden=true;
+
     Schedule schedule={};
     map<Course,Reservation> location={};
     const Preferences preferences;
     const Room max_rooms;
     const Timeslice max_timeslice;
-    set<Course> all_courses={};
+    map<Course,set<Course>> all_courses={};
+
+    friend ostream& operator<<(ostream& os, const Scheduler &s);
+
+
 };
 
 ostream& operator<<(ostream& os, const Scheduler &s)
 {
 
-    cout << "preferences:" << endl;
+    cout << "student preferences:" << endl;
+
     for(const auto& p : s.preferences)
     {
         cout << "student #" << p.first << "=> courses: ";
@@ -160,6 +221,7 @@ ostream& operator<<(ostream& os, const Scheduler &s)
         cout << "schedule found:" << endl;
         for(int r=0 ; r < s.max_rooms; r++)
         {
+
             os << "room #" << r << " ";
             for(int t=0 ; t <  s.max_timeslice ; t++ )
             {
@@ -186,14 +248,55 @@ ostream& operator<<(ostream& os, const Scheduler &s)
 
 void try_one(Scheduler::Preferences && preferences_, Scheduler::Room max_rooms_, Scheduler::Timeslice max_timeslice_)
 {
-    Scheduler s(forward<Scheduler::Preferences >(preferences_), max_rooms_, max_timeslice_);
-    s.buid_schedule();
+    Scheduler s(forward<Scheduler::Preferences >(preferences_), max_rooms_, max_timeslice_, true);
+    bool r=s.buid_schedule();
+#ifndef NDEBUG
+    {
+        Scheduler s2(forward<Scheduler::Preferences >(preferences_), max_rooms_, max_timeslice_, false);
+        bool r2=s2.buid_schedule();
+        assert(r2==r);
+        if (r)
+        {
+            assert(s.schedule == s2.schedule);
+        }
+    }
+#endif // NDEBUG
     cout << s;
 }
 
 int main()
 {
+    cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
     try_one({{0,{'A','B'}},{1,{'B','C'}},{2,{'C','D'}},{3,{'D','A'}}},2,2);
+    cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
     try_one({{0,{'A','B'}},{1,{'B','C'}},{2,{'C','D'}},{3,{'A','C'}}},2,2);
+    cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
+
+    std::default_random_engine generator;
+    generator.seed(0); // pseudo random generator with constant seed for repeatability
+
+    for(int repeat=0; repeat<10; repeat++) // try some set of generated student preferences
+    {
+        Scheduler::Preferences p;
+        const int nb_students=std::uniform_int_distribution<int>(5,7)(generator);
+        for(int i=0; i<nb_students; i++)
+        {
+            const int nb_courses = std::uniform_int_distribution<int>(2,4)(generator);
+            for(int j=0; j<nb_courses; j++)
+            {
+                Scheduler::Course c=std::uniform_int_distribution<Scheduler::Course> ('A','J')(generator);
+                p[i].insert(c);
+            }
+        }
+
+        for(int w=10; w>2; w--) // try same preferences with increasing time constraints
+        {
+            Scheduler::Preferences temp=p;
+            try_one(std::move(temp),10,w);
+            cout << "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
+        }
+
+    }
+
     return 0;
 }
